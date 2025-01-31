174847a9f1a5b120667be1a2b5aa653c422430bb
Nare33 <ulendaphaho@gmail.com>
2025-01-31 00:48:37 -0800

    Table list

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- task_3.sql
+
+-- No database selection needed here, it will be passed as a command-line argument.
+
+SHOW TABLES;  -- This lists all tables in the currently selected database.
+
+mysql -u your_user -p -h your_host alx_book_store < task_3.sql
