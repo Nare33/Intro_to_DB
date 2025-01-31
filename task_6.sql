cb8818a9a306e2e7d02830de7553b3eeeae8900e
Nare33 <ulendaphaho@gmail.com>
2025-01-31 02:46:42 -0800

    More database

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,12 @@
--- task_5.sql
--- This script inserts multiple rows into the 'customer' table
--- in the 'alx_book_store' database
+-- task_6.sql
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+USE alx_book_store;
 
-mysql -u your_username -p alx_book_store < task_5.sql
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
+mysql -u username -p database_name < task_6.sql
+
 
