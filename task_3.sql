acf0ffc8da9bf7c2a1389dbaf40e5d91cbdbe1c6
Nare33 <ulendaphaho@gmail.com>
2025-01-31 01:58:39 -0800

    Tables list

--- task_3.sql
+++ task_3.sql
@@ -3,7 +3,7 @@
 -- List all tables in the specified database
 SHOW TABLES;
 
-mysql -u your_username -p -D alx_book_store < task_3.sql
+mysql -u your_root -p -D alx_book_store < task_3.sql
 
 Intro_to_DB/
 │
